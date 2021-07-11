fcda2d331dddf64ee8bfd318a46bd92733eb1c39
rilestran <rilestran@gmail.com>
2021-07-11 20:39:38 +1000

    Added sign out button for ProfileFragment

--- app/src/main/java/com/example/infs3605/ProfileFragment.java
+++ app/src/main/java/com/example/infs3605/ProfileFragment.java
@@ -1,17 +1,25 @@
 package com.example.infs3605;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class ProfileFragment extends Fragment {
 
     private TextView profile;
+    private Button btSignOut;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -28,6 +36,34 @@ public class ProfileFragment extends Fragment {
 
         profile.setText("Profile Screen");
 
+        btSignOut = view.findViewById(R.id.btSignOut);
+
+        btSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
         return view;
     }
+
+    public void signOut() {
+        AuthUI.getInstance()
+                .signOut(getActivity())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        logOut();
+                    }
+                });
+    }
+    // Log out the user and prevent the user from accessing anything until logging in again
+    public void logOut() {
+        Intent intent = new Intent(getActivity().getApplicationContext(), LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+
+
 }
