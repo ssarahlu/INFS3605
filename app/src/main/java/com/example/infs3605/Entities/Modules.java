4be08d55ea730d18caf6c4fa49fbb4f56b29aab7
vanessalay <vanessaalay@gmail.com>
2021-07-23 12:15:04 +1000

    Added data

--- app/src/main/java/com/example/infs3605/Entities/Modules.java
+++ app/src/main/java/com/example/infs3605/Entities/Modules.java
@@ -78,6 +78,7 @@ public class Modules {
 
         modules.add(new Modules(0, "The Rainbow Serpent", "The meaning of art","The Rainbow Serpent or Rainbow Snake is creator of human beings. It has life-giving powers that send conception (fertility) spirits to all the watercourses, such as billabongs, rivers, creeks and lagoons as it is in control of producing rainfall.", "https://www.kullillaart.com.au/assets/images/image12.jpg", 0));
         modules.add(new Modules(1, "Test 2 The Rainbow Serpent", "2 The meaning of art","Test description 2",  "https://www.kullillaart.com.au/assets/images/image12.jpg", 0));
+        modules.add(new Modules(7, "Goo-Goor-Gaga the Kookaburra", "Music & Dance, Ceremonies","In this lesson we learn about the importance of music, dance, and ceremonies through the tale of Goo-Goor-Gaga the kookaburra. Goo-Goor-Gaga's helps the spirits by using his laughter to announce the arrival of the sun and the dawn of a new day.",  "https://www.kullillaart.com.au/assets/images/Kookaburra.jpg", 0));
 
         return modules;
     }
