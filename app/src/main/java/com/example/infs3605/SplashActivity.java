d71c0b55633553f0fdf42006810d36c61a16f865
Sarah Lu <56374523+ssarahlu@users.noreply.github.com>
2021-07-24 10:53:04 +1000

    [pls work]

--- app/src/main/java/com/example/infs3605/SplashActivity.java
+++ app/src/main/java/com/example/infs3605/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent homeIntent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
